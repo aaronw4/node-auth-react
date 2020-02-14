5043780267423d2a146aaf9405743351318a1395
Aaron Williams <aaronrw4@yahoo.com>
2020-02-13 17:03:28 -0800

    added private route to users page

--- users/src/App.js
+++ users/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {UserPrivateRoute} from './components/privateRoute';
 import Login from './components/login';
 import Users from './components/users';
+import { UserPrivateRoute } from './components/privateRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Route exact path='/' component={Login}/>
-        <Route path='/users' component={Users}/>
+        <UserPrivateRoute path='/users' component={Users}/>
       </div>
     </BrowserRouter>
   );
