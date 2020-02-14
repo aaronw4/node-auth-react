3c6f8fff94c5e87f4c5471d18f2f94ff3b2846b2
Aaron Williams <aaronrw4@yahoo.com>
2020-02-13 17:01:30 -0800

    created privateRoute.js

---
+++ users/src/components/privateRoute.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+export const UserPrivateRoute = ({component: Component, ...theRest}) => {
+    return (
+        <Route
+            {...theRest}
+            render = {() => {
+                if (localStorage.getItem('token')) {
+                    return <Component/>
+                } else {
+                    return <Redirect to='/'/>
+                }
+            }}
+        />
+    )
+}
