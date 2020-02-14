8b500192289857ca1bdac53c055cf49ee70e7e00
Aaron Williams <aaronrw4@yahoo.com>
2020-02-13 16:39:21 -0800

    created login.js

---
+++ users/src/components/login.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+    
+    handleChange = e => {
+        this.state({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        })
+    };
+
+    login = e => {
+        e.preventDefault();
+
+        axios
+            .post('https://arw-node-auth.herokuapp.com/users/login', this.state.credentials)
+            .then(res => {
+                localStorage.setItem('token', res.data.token);
+                this.props.history.push('/users')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    };
+
+    render() {
+        return (
+            <div>
+                <h1 className='loginTitle'>Users Log In Page</h1>
+
+                <form onSubmit={this.login}>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                        placeholder='User Name'
+                        className='loginInput'
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                        placeholder='Password'
+                        className='loginInput'
+                    />
+                    <button>Log In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login
