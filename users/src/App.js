f6420a74c7d5ef2b5e7e7e26070a0e4ad9427743
Aaron Williams <aaronrw4@yahoo.com>
2020-02-17 22:55:42 -0800

    Created route to register page

--- users/src/App.js
+++ users/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Login from './components/login';
 import Users from './components/users';
+import {Register} from './components/register';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Route exact path='/' component={Login}/>
         <Route path='/users' component={Users}/>
+        <Route path='/register' component={Register}/>
       </div>
     </BrowserRouter>
   );
